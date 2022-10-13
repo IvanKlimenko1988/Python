226e98517b2f93079f3c644e7f7bfd0345f2c854
Ivan <i.klimenko.eng@gmail.com>
2022-10-14 02:30:11 +0300

    Task_5 started

--- Seminars/Seminar_1/Home_work_1/Task_5.py
+++ Seminars/Seminar_1/Home_work_1/Task_5.py
@@ -8,3 +8,28 @@
 # 1 2 3 4
 # 5 7 9 10
 
+
+rows = int(input("Введите количество строк: "))
+columns = int(input("Введите количество столбцов: "))
+array_num = []
+
+for i in range(rows):
+    temp = []
+    for i in range(columns):
+        temp.append(int(input('Введите значение элемента списка: ')))
+    array_num.append(temp)
+for i in array_num:
+    print(i)
+
+for run in range((rows*columns)-1):
+    for i in range(rows):
+        for j in range(columns-1-run):
+            if array_num[i][j] > array_num[i][j+1]:
+                array_num[i][j], array_num[i][j+1] = array_num[i][j+1], array_num[i][j]
+print()
+
+for arr in array_num:
+    for el in arr:
+        print(el, end=' ')
+    print()
+
