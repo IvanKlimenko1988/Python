cafb121e0340e3da30c68bc53bd94d3069903b79
Ivan <i.klimenko.eng@gmail.com>
2022-10-14 08:55:32 +0400

    Changes added

--- Seminars/Seminar_1/Home_work_1/Task_5.py
+++ Seminars/Seminar_1/Home_work_1/Task_5.py
@@ -8,3 +8,13 @@
 # 1 2 3 4
 # 5 7 9 10
 
+list = [[1, 4, 7, 2], [5, 9, 10, 3]]
+
+row = int(input('Введите количество строк'))
+columns = int(input('Введите количество столбцов'))
+
+for i in range(row):
+    for j in range(columns):
+        print(list[i][j], end=' ')
+
+
