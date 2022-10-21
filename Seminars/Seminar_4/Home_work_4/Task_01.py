19cd907dc1f3314fc09e2b56d6625f653a985d20
Ivan <i.klimenko.eng@gmail.com>
2022-10-21 23:17:02 +0400

    Task_1 started

--- Seminars/Seminar_4/Home_work_4/Task_01.py
+++ Seminars/Seminar_4/Home_work_4/Task_01.py
@@ -1,1 +1,40 @@
 # Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+
+
+
+n = int(input('Введите натуральное число: '))
+res = []
+if n > 0:
+# Условия делимости
+# 1-если число чётное, то делитель 2:
+    prostoe = 2
+    if n % 2 == 0:
+        a = n
+        count = 0
+        while n > 1:
+            if a % 2 == 0:
+                a = a / prostoe
+                res.append(int(a))
+                n /= prostoe
+                count += 1
+            else:
+                break
+        print(res)
+        print(count)
+    else: 
+        prostoe +=1
+        print('Число нечётное')
+else:
+    print('Введите целое число N > 0')
+
+
+# sum = 0
+# while n > 0:
+#     sum += n % 10
+#     n //= 10
+# print(sum)
+# if n / n == 1 and n / 1 == n:
+#     print('простое')
+# else:
+#     print('нет')
