322c7e1c15a24bff974f4ce05c6ad655f8a9ed3b
Ivan <i.klimenko.eng@gmail.com>
2022-10-23 22:44:01 +0300

    Task_03 Home_work_4 added

--- Seminars/Seminar_4/Home_work_4/Task_03.py
+++ Seminars/Seminar_4/Home_work_4/Task_03.py
@@ -1,25 +1,71 @@
 # Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
-# *Пример:* 
+# *Пример:*
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
-
-
 
 from random import randint, random
 
 
-k = int(input('Задайте степень:'))
+def create_ratio_list(num):
+    ratio = []
+    while num > 0:
+        ratio.append(randint(0, 100))
+        num -= 1
+    return ratio
 
-list_ratio = []
-while k > 0:
-    list_ratio.append(randint(0, 100))
-    list_ratio.append('x')
-    list_ratio.append('^')
-    list_ratio.append(k)
-    list_ratio.append(' + ')
-    k -= 1
+def create_polynomial(ratio_list, k):
+    polynomial = []
 
-list_ratio.append(randint(0, 100))
-list_ratio.append(' = 0')
-print(''.join(map(str, list_ratio)))
+    for i in range(len(ratio_list)):
+        if ratio_list[i] == 0:
+            continue
+        elif ratio_list[i] == 1:
+            polynomial.append('x')
+            if k == 1:
+                polynomial.append(' + ')
+            else:
+                polynomial.append('^')
+                polynomial.append(k)
+                polynomial.append(' + ')
+                k -= 1
+        else:
+            polynomial.append(ratio_list[i])
+            polynomial.append('x')
+            if k != 1:
+                polynomial.append('^')
+                polynomial.append(k)
+                polynomial.append(' + ')
+                k -= 1
+            else:
+                polynomial.append(' + ')
+
+    polynomial.append(randint(0, 100))
+
+    if polynomial[-1] == 0:
+        polynomial.remove(0)
+        polynomial.remove(' + ')
+    polynomial.append(' = 0')
+    return polynomial
+
+
+def save():
+    with open('Многочлен.txt', "w") as data:
+        data.write(list_1)
+        print("Успешное сохранение в Многочлен.txt")
+        data.close()
+
+
+try:
+    print('Для создания многочлена введите степень')
+    k = int(input('Задайте натуральную степень: '))
+    if k <= 0:
+        print('Введите целое число больше 0')
+    else:    
+        list_k = create_ratio_list(k)
+        result_polynomial = create_polynomial(list_k, k)
+        list_1 = ''.join(map(str, result_polynomial))
+        print(list_1)
+        save()    
+except:
+    print('Введите целое число больше 0')
 
 
