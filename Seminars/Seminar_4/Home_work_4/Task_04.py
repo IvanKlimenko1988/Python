f5f9ac0d21a50a7a570d5c46efefae649c915240
Ivan <i.klimenko.eng@gmail.com>
2022-10-26 09:50:34 +0400

    Task_04 added

--- Seminars/Seminar_4/Home_work_4/Task_04.py
+++ Seminars/Seminar_4/Home_work_4/Task_04.py
@@ -17,32 +17,26 @@
 # Если D > 0, корней будет два.
 
 import re
-
-
+print('Программа для решения квадратных уравнений')
 numbers = re.compile('-?\d+')
-s = ''.join(input("Введите элементы уравнение через пробел: ").split()).lower()
+s = ''.join(input("Введите элементы уравнение, например: '6*x^2+5*x+6=0': ").split()).lower()
 s = s.replace('^2', '')
 s = s.replace('=0', '')
-# ch_s = '-x'
-
-# if ch_s in s:
-#     s = s.replace(ch_s, '-1')
 
 sum_min = 0
 result = list(map(int, numbers.findall(s)))
-print(result)
-
 
 sum_min = 0
 sum_plus = 0
+
 for i in range(len(s)):
     sum_x = s.count('x')
     sum_min = s.count('-')
     sum_plus = s.count('+')
-print(f"'x' = {sum_x}")
-print(f"'-' = {sum_min}")
-print(f"'+' = {sum_plus}")
 
+ch_minus = 'x-x' 
+ch_plus = 'x+x' 
+ch_xmul = '*x'
 
 if sum_x == 2 and sum_min == 0 and sum_plus == 2 and len(result) == 3:
     print(result)
@@ -64,22 +58,38 @@ elif sum_x == 2 and sum_min == 2 and sum_plus == 1 and len(result) == 2:
     temp = result[1]
     result[1] = result[-1]
     result[-1] = temp
-elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 0:
-    result.append(-1)
+elif sum_x == 2 and sum_min == 2 and sum_plus == 0 and len(result) == 2:
     result.append(1)
-    result.append(0)    
-elif sum_x == 2 and sum_min == 1 and sum_plus == 0 and len(result) == 1:
-    result.append(1)
-    result.append(0)
-    temp = result[0]
-    result[0] = result[1]
-    result[1] = temp
+    result.reverse()  
+    temp = result[1]
+    result[1] = result[-1]
+    result[-1] = temp
 elif sum_x == 2 and sum_min == 1 and sum_plus == 2 and len(result) == 2:
     result.append(-1)
     result.reverse()  
     temp = result[1]
     result[1] = result[-1]
+    result[-1] = temp 
+elif sum_x == 2 and sum_min == 1 and sum_plus == 0 and len(result) == 1 and ch_minus in s:
+    result.append(-1)
+    result.append(0)
+elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 1 and ch_minus in s:
+    result.append(1)
+    result.append(0)  
+    temp = result[1]
+    result[1] = result[-1]
     result[-1] = temp
+    result.reverse() 
+elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 1 and ch_plus in s:
+    result.append(1)
+    result.append(0)  
+elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 1:
+    result.append(1)
+    result.append(1)  
+    temp = result[1]
+    result[1] = result[-1]
+    result[-1] = temp
+    result.reverse() 
 elif sum_x == 1 and sum_min == 1 and sum_plus == 1 and len(result) == 1:
     result.append(-1)
     result.append(0) 
@@ -89,6 +99,11 @@ elif sum_x == 1 and sum_min == 1 and sum_plus == 1 and len(result) == 1:
     temp = result[0]
     result[0] = result[1]
     result[1] = temp
+elif sum_x == 1 and sum_min == 1 and sum_plus == 0 and len(result) == 2:
+    result.append(0) 
+    temp = result[1]
+    result[1] = result[-1]
+    result[-1] = temp
 elif sum_x == 1 and sum_min == 2 and sum_plus == 0 and len(result) == 1:
     result.append(-1)
     result.append(0) 
@@ -98,28 +113,43 @@ elif sum_x == 1 and sum_min == 2 and sum_plus == 0 and len(result) == 1:
     temp = result[0]
     result[0] = result[1]
     result[1] = temp
+elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 0:
+    result.append(-1)
+    result.append(1)
+    result.append(0)
+elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 0:
+    result.append(1)
+    result.append(1)
+    result.append(0)
+elif sum_x == 2 and sum_min == 2 and sum_plus == 0 and len(result) == 0:
+    result.append(-1)
+    result.append(-1)
+    result.append(0)
 elif sum_x == 2 and sum_min == 0 and sum_plus == 2:
     result.append(1)
     result.reverse()  
     temp = result[1]
     result[1] = result[-1]
     result[-1] = temp
-elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 0:
-    result.append(1)
-    result.append(1)
+elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 2:
     result.append(0)
-elif sum_x == 2 and sum_min == 0 and sum_plus == 1:
+elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 1 and ch_xmul in s:
+    result.append(0)
+    result.append(1)
+    temp = result[0]
+    result[0] = result[1]
+    result[1] = temp
+    result.reverse()  
+elif sum_x == 2 and sum_min == 1 and sum_plus == 0 and len(result) == 1 and ch_xmul in s:
     result.append(1)
     result.append(0)
     temp = result[0]
     result[0] = result[1]
     result[1] = temp
-elif sum_x == 2 and sum_min == 2 and sum_plus == 0 and len(result) == 2:
+elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 1:
     result.append(1)
-    result.reverse()  
-    temp = result[1]
-    result[1] = result[-1]
-    result[-1] = temp
+    result.append(0)
+        
 elif sum_x == 2 and sum_min == 1 and sum_plus == 0:
     result.append(1)
     result.append(-1)
@@ -139,27 +169,77 @@ elif sum_x == 1 and sum_min == 1 and sum_plus == 1:
     temp = result[1]
     result[1] = result[-1]
     result[-1] = temp
-
-for i in range(len(s)):
-    print(s[i], end= '')
-print()
-    
-    
-if sum_x == 0 and len(result) == 1:
-    print("Уравнение имеет один корень x = 0")    
-if sum_x == 1 and len(result) == 0:    
+elif sum_x == 1 and sum_min == 1 and sum_plus == 0 and len(result) == 1:
+    print("Уравнение имеет один корень x = 0")
+elif sum_x == 1 and sum_min == 0 and sum_plus == 0 and len(result) == 1:
     print("Уравнение имеет один корень x = 0")
 
-print(result)
-# 6*x^2+5*x+6=0
-# x^2+5*x+6=0
-# x^2+9*x=0
-
-# x^2+x=0
-
-# 2*x^2+2=0
+if sum_x == 0 and len(result) == 1:
+    print("Уравнение имеет один корень x = 0")
+        
+if sum_x == 1 and len(result) == 0:    
+    print("Уравнение имеет один корень x = 0")
 
 len_str = len(s)
 if s.count('0*x^2', 0, len_str-2) or s.count('0x^2', 0, len_str-2):
     print("Уравнение не имеет решения, а = 0")
+    exit()
+
+# ----------------------------------------------
+# Нашли все возможные коэффициенты a, b и с!!
+# ----------------------------------------------
+
+a = result[0]
+b = result[1]
+c = result[2]
+x1 = 0
+x2 = 0
+
+# ---------------------------------------------------
+# Неполные квадратные уравнения, если b = 0 или c = 0
+# ---------------------------------------------------
+
+# Если b = 0
+if b == 0:
+    if -c / a >= 0: # Если выполняется это условие,корней будет два.
+        x1 = -(-c / a)**0.5
+        x2 = (-c / a)**0.5
+        print('Ответ:')
+        print(f'x1 = {x1}')
+        print(f'x2 = {x2}')
+        exit()
+
+    elif -c / a < 0: # Корней нет.
+        print('Ответ: корней нет')
+        exit()
+
+elif c == 0:
+        # Если с = 0, корней будет два.
+        x1 = 0
+        x2 = -b / a
+        print('Ответ:')
+        print(f'x1 = {x1}')
+        print(f'x2 = {x2}')
+        exit()
+
+discriminant = b**2 - 4 * a * c
+print(f'D = {discriminant}')
+
+if discriminant < 0: # Если D < 0, корней нет
+    print('D < 0')
+    print('Ответ: корней нет')
+    exit()
+
+elif discriminant == 0: # Если D = 0, есть ровно один корень
+    x1 = (-b + discriminant**0.5) / 2 * a
+    print('D = 0, есть ровно один корень')
+    print(f'Ответ: x1 = {x1}')
+    exit()
+
+elif discriminant > 0: #Если D > 0, корней будет два.
+    x1 = (-b + discriminant**0.5) / 2 * a
+    x2 = (-b - discriminant**0.5) / 2 * a
+    print('D > 0, корней будет два')
+    print(f'x1 = {x1}')
+    print(f'x2 = {x2}')
     exit()
