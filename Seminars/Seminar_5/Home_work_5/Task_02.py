c8b36fb0ea5e1cedb16392e6eadea2e6a571345c
Ivan <i.klimenko.eng@gmail.com>
2022-10-29 21:48:44 +0400

    Task_02 def added

--- Seminars/Seminar_5/Home_work_5/Task_02.py
+++ Seminars/Seminar_5/Home_work_5/Task_02.py
@@ -1,52 +1,47 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
-# numbers = int(print("Введите последовательность чисел: "))
+import re
+
+def rle_compression(text):
+    count = 1
+    rle_str = ''
+    i = 0
+    while i < len(text):
+        for j in range(len(text) - 1):
+            if i >= len(text):
+                break
+            else:
+                if text[i] == text[j + 1]:
+                    count += 1
+                    i += 1
+                else:
+                    rle_str += str(count) + text[i]
+                    count = 1
+                    i += 1
+    return rle_str
+
+def rle_recovery(text):
+    res_str = ''
+    num_filtr = re.compile('-?\d+')
+    list_num = list(map(int, num_filtr.findall(code)))
+    j = 0
+    for i in range(len(code)):
+        if not code[i].isdigit():
+            res_str += str(code[i]) * list_num[j]
+            j += 1
+    return res_str
+
+inmort_string = 'AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE'
 
 
-# numbers = 10000100001111111110111110000000000000011111111
-numbers = 1011
-str_1 = str(numbers)
-count = 1
-num = []
-cnt = []
 
-# for i in range(len(str_1)-1):
-#     if i <= len(str_1):
-#         if str_1[i] == str_1[i + 1]:
-#             count +=1
-#             num.append(str_1[i])
-            
-#         else:
-            
-#             cnt.append(count)
-#             num.append(str_1[i])
-#             count = 1
-          
-            
-# print(len(str_1))
-# print(len(num))
-# print(num)
-# print(cnt)
-       
-numbers = 10000100001111111110111110000000000000011111111
-str_1 = str(numbers)
-cout = 1
-for i in range(len(str_1)-1):
-    if i <= len(str_1):
-        if str_1[i] == str_1[i + 1]:
-            cout += 1
-            num.append(str_1[i])
-        else:
-            a = str_1[i]
-            print(cout, str_1[i])
-            cnt.append(count)
-            num.append(str_1[i])
-            cout = 1
-print(cout, str_1[i])
+code = rle_compression(inmort_string)
+print(code)
 
-            
-print(len(str_1))
-print(len(num))
-print(num)
-print(cnt)
+de_code = rle_recovery(code)
+print(de_code)
+
+
+
+
