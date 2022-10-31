a4ae8a6a63ae4db934458e74ae90b7656ec5078b
Ivan <i.klimenko.eng@gmail.com>
2022-10-31 18:15:11 +0400

    Task_03 added

--- Seminars/Seminar_5/Home_work_5/Task_03.py
+++ Seminars/Seminar_5/Home_work_5/Task_03.py
@@ -1,1 +1,48 @@
 # Напишите программу, удаляющую из текста все слова, содержащие "абв". Функции FIND и COUNT юзать нельзя.
+# Вариант №1 Исходный текс переводи нижний регистр
+
+# def del_words(text):
+#     list_words = text.split()
+#     i = 0
+#     while i < (len(list_words)):
+#         if 'абв' in list_words[i]:
+#             list_words.pop(i)
+#         else:
+#             i +=1
+#     return ' '.join(map(str, list_words))
+
+
+# print('Привет, удалим все слова, содержащие "абв".')
+# start_string = input('Введите текст: ').lower()
+# res_str = del_words(start_string)
+# print('Текст после удаления: ')
+# print(res_str)
+
+# Вариант № 2 Исходный текс не переводим весь в нижний регистр
+# s = 'Привет, удалим все слова, содержащие "абв".'
+
+def del_words(text):
+    ch = 'абв'
+    ch_1 = 'Абв'
+    ch_2 = 'аБв'
+    ch_3 = 'абВ'
+    ch_4 = 'АБВ'
+    ch_5 = 'АБв'
+    ch_6 = 'аБВ'
+    ch_7 = 'АбВ'
+    list_words = text.split()
+    i = 0
+    while i < len(list_words):
+        if ch in list_words[i] or ch_1 in list_words[i] or ch_2 in list_words[i] or ch_3 in list_words[i] or ch_4 in list_words[i] or ch_5 in list_words[i] or ch_6 in list_words[i] or ch_7 in list_words[i]:
+            list_words.pop(i)
+        else:
+            i +=1
+    return ' '.join(map(str, list_words))
+
+print('Привет, удалим все слова, содержащие "абв".')
+start_string = input('Введите текст: ')
+res_str = del_words(start_string)
+print('Текст после удаления: ')
+print(res_str)
+
+
--- Seminars/Seminar_5/Home_work_5/Task_04.py
+++ Seminars/Seminar_5/Home_work_5/Task_04.py
@@ -1,2 +1,176 @@
 # Даны два многочлена. Задача - сформировать их сумму.
 # например, 5*x^3 + 2*x^2 + 6 и 7*x^2+6*x+3 , Тогда их сумма будет равна 5*x^3 + 9*x^2 + 6*x + 9
+import re
+
+def find_a_b_c(str):
+
+    numbers = re.compile('-?\d+')
+    # str = ''.join(input("Введите элементы уравнение, например: '6*x^2+5*x+6=0': ").split()).lower()
+    str = str.replace('^2', '')
+    str = str.replace('=0', '')
+    str = str.replace('^3', '')
+
+    result = list(map(int, numbers.findall(str)))
+    print(result)
+    sum_min = 0
+    sum_plus = 0
+
+    for i in range(len(str)):
+        sum_x = str.count('x')
+        sum_min = str.count('-')
+        sum_plus = str.count('+')
+
+    ch_minus = 'x-x' 
+    ch_plus = 'x+x' 
+    ch_xmul = '*x'
+
+    if sum_x == 2 and sum_min == 0 and sum_plus == 2 and len(result) == 3:
+        print(f'Коэффициенты : {result}')
+    elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 2:
+        result.append(1)
+        result.reverse()  
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp
+    elif sum_x == 2 and sum_min == 3 and sum_plus == 0 and len(result) == 2:
+        result.append(-1)
+        result.reverse()  
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp
+    elif sum_x == 2 and sum_min == 2 and sum_plus == 1 and len(result) == 2:
+        result.append(-1)
+        result.reverse()  
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp
+    elif sum_x == 2 and sum_min == 2 and sum_plus == 0 and len(result) == 2:
+        result.append(1)
+        result.reverse()  
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp
+    elif sum_x == 2 and sum_min == 1 and sum_plus == 2 and len(result) == 2:
+        result.append(-1)
+        result.reverse()  
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp 
+    elif sum_x == 2 and sum_min == 1 and sum_plus == 0 and len(result) == 1 and ch_minus in str:
+        result.append(-1)
+        result.append(0)
+    elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 1 and ch_minus in str:
+        result.append(1)
+        result.append(0)  
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp
+        result.reverse() 
+    elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 1 and ch_plus in str:
+        result.append(1)
+        result.append(0)  
+    elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 1:
+        result.append(1)
+        result.append(1)  
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp
+        result.reverse() 
+    elif sum_x == 1 and sum_min == 1 and sum_plus == 1 and len(result) == 1:
+        result.append(-1)
+        result.append(0) 
+        temp = result[0]
+        result[0] = result[-1]
+        result[-1] = temp
+        temp = result[0]
+        result[0] = result[1]
+        result[1] = temp
+    elif sum_x == 1 and sum_min == 1 and sum_plus == 0 and len(result) == 2:
+        result.append(0) 
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp
+    elif sum_x == 1 and sum_min == 2 and sum_plus == 0 and len(result) == 1:
+        result.append(-1)
+        result.append(0) 
+        temp = result[0]
+        result[0] = result[-1]
+        result[-1] = temp
+        temp = result[0]
+        result[0] = result[1]
+        result[1] = temp
+    elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 0:
+        result.append(-1)
+        result.append(1)
+        result.append(0)
+    elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 0:
+        result.append(1)
+        result.append(1)
+        result.append(0)
+    elif sum_x == 2 and sum_min == 2 and sum_plus == 0 and len(result) == 0:
+        result.append(-1)
+        result.append(-1)
+        result.append(0)
+    elif sum_x == 2 and sum_min == 0 and sum_plus == 2:
+        result.append(1)
+        result.reverse()  
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp
+    elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 2:
+        result.append(0)
+    elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 1 and ch_xmul in str:
+        result.append(0)
+        result.append(1)
+        temp = result[0]
+        result[0] = result[1]
+        result[1] = temp
+        result.reverse()  
+    elif sum_x == 2 and sum_min == 1 and sum_plus == 0 and len(result) == 1 and ch_xmul in str:
+        result.append(1)
+        result.append(0)
+        temp = result[0]
+        result[0] = result[1]
+        result[1] = temp
+    elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 1:
+        result.append(1)
+        result.append(0)
+            
+    elif sum_x == 2 and sum_min == 1 and sum_plus == 0:
+        result.append(1)
+        result.append(-1)
+        result.append(0)
+    elif sum_x == 1 and sum_min == 0 and sum_plus == 1:
+        result.append(0)
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp
+    elif sum_x == 1 and sum_min == 2 and sum_plus == 0:
+        result.append(0)
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp
+    elif sum_x == 1 and sum_min == 1 and sum_plus == 1:
+        result.append(0)
+        temp = result[1]
+        result[1] = result[-1]
+        result[-1] = temp
+    return result[0] ,result[1], result[2]
+
+
+try:
+    print('Программа для решения квадратных уравнений')
+    str = ''.join(input("Введите элементы уравнение, например: 'a*x^2+b*x+6=0': ").split()).lower()
+    ch_x = 'x'
+    ch_p ='+'
+    ch_m = '-'
+    if ch_x in str or ch_p in str or ch_m in str:
+        a , b ,c = find_a_b_c(str)
+        print(a)
+        print(b)
+        print(c)
+        
+    else:
+        print('Программа завершила свою работу, неправильный ввод!')
+except:
+    print("Завершение программы")
