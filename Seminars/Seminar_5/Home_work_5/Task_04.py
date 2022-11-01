8e508b0da9c6a365d560a2b44dbae4d8813ed6e1
Ivan <i.klimenko.eng@gmail.com>
2022-11-01 16:15:25 +0400

    Task_04 and Task_05 changed

--- Seminars/Seminar_5/Home_work_5/Task_04.py
+++ Seminars/Seminar_5/Home_work_5/Task_04.py
@@ -2,175 +2,97 @@
 # например, 5*x^3 + 2*x^2 + 6 и 7*x^2+6*x+3 , Тогда их сумма будет равна 5*x^3 + 9*x^2 + 6*x + 9
 import re
 
-def find_a_b_c(str):
+"""задача 4 необязательная Даны два многочлена. Задача - сформировать их сумму.
+например, 5*x^3 + 2*x^2 + 6 и 7*x^2+6*x+3 , Тогда их сумма будет равна 5*x^3 + 9*x^2 + 6*x + 9"""
 
-    numbers = re.compile('-?\d+')
-    # str = ''.join(input("Введите элементы уравнение, например: '6*x^2+5*x+6=0': ").split()).lower()
-    str = str.replace('^2', '')
-    str = str.replace('=0', '')
-    str = str.replace('^3', '')
+def max_degree(poly):
+    list = poly.replace(' ', '').split('x')
+    degree = 0
+    check = 0
+    for i in list:
+        for l in range(len(i)):
+            if i[l] == '^':
+                check = 1
+                degree = int(i[l + 1])
+                break
+        if check == 1:
+            check = 0
+            break
+    return degree
 
-    result = list(map(int, numbers.findall(str)))
-    print(result)
-    sum_min = 0
-    sum_plus = 0
+def list_of_coefficients(poly1, degree):
+    list = poly1.split('+')
+    new_list = []
+    d = degree
+    i = 0
+    while d > 0:
+        if i < len(list):
+            if not 'x' in list[i]:
+                new_list.append(0)
+            else:
+                if list[i][0] == '-':
+                    if list[i][1].isdigit():
+                        temp = int(list[i][1]) * (-1)
+                    elif list[i][1] == 'x':
+                        temp = -1
+                elif list[i][0].isdigit():
+                    temp = int(list[i][0])
+                elif list[i][0] == 'x':
+                    temp = 1
+                for l in range(len(list[i])):
+                    if list[i][l] == 'x':
+                        if ('^' in list[i]) and (list[i][l + 1] == '^'):
+                            if list[i][l + 2] == str(d):
+                                new_list.append(temp)
+                                i += 1
+                            else:
+                                new_list.append(0)
+                            break
+                        else:
+                            if '1' == str(d):
+                                new_list.append(temp)
+                                i += 1
+                            else:
+                                new_list.append(0)
+                            break
+        else:
+            new_list.append(0)
+            new_list.append(0)
+        d -= 1
+    for j in list:
+        if not 'x' in j:
+            new_list.append(int(j))
+    return new_list
 
-    for i in range(len(str)):
-        sum_x = str.count('x')
-        sum_min = str.count('-')
-        sum_plus = str.count('+')
+def list_to_polynomial(list1):
+    res = ''
+    degree = len(list1) - 1
+    for i in list1:
+        if i != 0:
+            if degree == 1:
+                res += f'{str(i)}*x^{degree} + '
+                degree -= 1
+            elif degree == 0:
+                res += f'{str(i)}'
+            else:
+                res += f'{str(i)}*x^{degree} + '
+                degree -= 1
+        else:
+            degree -= 1
 
-    ch_minus = 'x-x' 
-    ch_plus = 'x+x' 
-    ch_xmul = '*x'
-
-    if sum_x == 2 and sum_min == 0 and sum_plus == 2 and len(result) == 3:
-        print(f'Коэффициенты : {result}')
-    elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 2:
-        result.append(1)
-        result.reverse()  
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp
-    elif sum_x == 2 and sum_min == 3 and sum_plus == 0 and len(result) == 2:
-        result.append(-1)
-        result.reverse()  
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp
-    elif sum_x == 2 and sum_min == 2 and sum_plus == 1 and len(result) == 2:
-        result.append(-1)
-        result.reverse()  
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp
-    elif sum_x == 2 and sum_min == 2 and sum_plus == 0 and len(result) == 2:
-        result.append(1)
-        result.reverse()  
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp
-    elif sum_x == 2 and sum_min == 1 and sum_plus == 2 and len(result) == 2:
-        result.append(-1)
-        result.reverse()  
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp 
-    elif sum_x == 2 and sum_min == 1 and sum_plus == 0 and len(result) == 1 and ch_minus in str:
-        result.append(-1)
-        result.append(0)
-    elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 1 and ch_minus in str:
-        result.append(1)
-        result.append(0)  
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp
-        result.reverse() 
-    elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 1 and ch_plus in str:
-        result.append(1)
-        result.append(0)  
-    elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 1:
-        result.append(1)
-        result.append(1)  
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp
-        result.reverse() 
-    elif sum_x == 1 and sum_min == 1 and sum_plus == 1 and len(result) == 1:
-        result.append(-1)
-        result.append(0) 
-        temp = result[0]
-        result[0] = result[-1]
-        result[-1] = temp
-        temp = result[0]
-        result[0] = result[1]
-        result[1] = temp
-    elif sum_x == 1 and sum_min == 1 and sum_plus == 0 and len(result) == 2:
-        result.append(0) 
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp
-    elif sum_x == 1 and sum_min == 2 and sum_plus == 0 and len(result) == 1:
-        result.append(-1)
-        result.append(0) 
-        temp = result[0]
-        result[0] = result[-1]
-        result[-1] = temp
-        temp = result[0]
-        result[0] = result[1]
-        result[1] = temp
-    elif sum_x == 2 and sum_min == 1 and sum_plus == 1 and len(result) == 0:
-        result.append(-1)
-        result.append(1)
-        result.append(0)
-    elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 0:
-        result.append(1)
-        result.append(1)
-        result.append(0)
-    elif sum_x == 2 and sum_min == 2 and sum_plus == 0 and len(result) == 0:
-        result.append(-1)
-        result.append(-1)
-        result.append(0)
-    elif sum_x == 2 and sum_min == 0 and sum_plus == 2:
-        result.append(1)
-        result.reverse()  
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp
-    elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 2:
-        result.append(0)
-    elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 1 and ch_xmul in str:
-        result.append(0)
-        result.append(1)
-        temp = result[0]
-        result[0] = result[1]
-        result[1] = temp
-        result.reverse()  
-    elif sum_x == 2 and sum_min == 1 and sum_plus == 0 and len(result) == 1 and ch_xmul in str:
-        result.append(1)
-        result.append(0)
-        temp = result[0]
-        result[0] = result[1]
-        result[1] = temp
-    elif sum_x == 2 and sum_min == 0 and sum_plus == 1 and len(result) == 1:
-        result.append(1)
-        result.append(0)
-            
-    elif sum_x == 2 and sum_min == 1 and sum_plus == 0:
-        result.append(1)
-        result.append(-1)
-        result.append(0)
-    elif sum_x == 1 and sum_min == 0 and sum_plus == 1:
-        result.append(0)
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp
-    elif sum_x == 1 and sum_min == 2 and sum_plus == 0:
-        result.append(0)
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp
-    elif sum_x == 1 and sum_min == 1 and sum_plus == 1:
-        result.append(0)
-        temp = result[1]
-        result[1] = result[-1]
-        result[-1] = temp
-    return result[0] ,result[1], result[2]
+    res = res.replace('+ -', '- ')
+    print(res)
 
 
-try:
-    print('Программа для решения квадратных уравнений')
-    str = ''.join(input("Введите элементы уравнение, например: 'a*x^2+b*x+6=0': ").split()).lower()
-    ch_x = 'x'
-    ch_p ='+'
-    ch_m = '-'
-    if ch_x in str or ch_p in str or ch_m in str:
-        a , b ,c = find_a_b_c(str)
-        print(a)
-        print(b)
-        print(c)
-        
-    else:
-        print('Программа завершила свою работу, неправильный ввод!')
-except:
-    print("Завершение программы")
+polynomial_first = ' 5*x^3 + 2*x^2 + 6'.replace('х', 'x').replace(' ', '').replace('*', '').replace('-','+-')
+polynomial_second = ' 7*x^2 - x + 3'.replace('х', 'x').replace(' ', '').replace('*', '').replace('-','+-')
+degree1 = max_degree(polynomial_first)
+degree2 = max_degree(polynomial_second)
+maximum_degree = max(degree1, degree2)
+list1 = list_of_coefficients(polynomial_first, maximum_degree)
+list2 = list_of_coefficients(polynomial_second, maximum_degree)
+res_list = [x + y for x, y in zip(list1, list2)]
+list_to_polynomial(res_list)
+
+"""задача 4 необязательная Даны два многочлена. Задача - сформировать их сумму.
+например, 5*x^3 + 2*x^2 + 6 и 7*x^2+6*x+3 , Тогда их сумма будет равна 5*x^3 + 9*x^2 + 6*x + 9"""
--- Seminars/Seminar_5/Home_work_5/Task_05.py
+++ Seminars/Seminar_5/Home_work_5/Task_05.py
@@ -40,7 +40,7 @@ import re
 
 s = '5*x^3+2*x^2+6=0'
 
-s1 = '7*x^2+6*x+3=0'
+s1 = '7*x^2+6*x^1+3=0'
 
 s = s.replace(' ', '')
 s1 = s1.replace(' ', '')
@@ -53,31 +53,19 @@ print(s1)
 
 
 result_x = re.findall(r'\S\^[1-9]', s)   #['x^3', 'x^2', 'x^4']
-result_x1 = re.findall(r'\S\^[1-9]', s1)   #['x^3', 'x^2', 'x^4']
-# result_x1 = re.findall(r'x[^+]', s1)   #['x^3', 'x^2', 'x^4']
-
+result_x1 = re.findall(r'\S\^[1-9]', s1)  
 
 result_c1 = re.findall(r'\-?\d+', s1)
 s = s.replace('x^3', '')
 s = s.replace('x^2', '')
 s1 = s1.replace('x^2', '')
+s1 = s1.replace('x^1', '')
 
 numbers = re.compile('-?\d+')
 result_num = list(map(int, numbers.findall(s)))
 result_num1 = list(map(int, numbers.findall(s1)))
-
-
-
-
-# print(result_x)
-# print(result_x1)
-
-
-
-
-
-# print(result_num)
-# print(result_num1)
+print(result_num)
+print(result_num1)
 
 
 list_1 = list(zip(result_num, result_x))
@@ -85,39 +73,40 @@ list_2 = list(zip(result_num1, result_x1))
 print(f'Первое уравнение: {list_1}')
 print(f'Второе уравнение: {list_2}')
 
-# c = int(result_c[-1])
-# c1 = int(result_c1[-1])
-# print(f'Свободный член: {c}')
-# print(f'Свободный член: {c1}')
-
-
-# например, 5*x^3 + 2*x^2 + 6 и 7*x^2+6*x+3 , Тогда их сумма будет равна 5*x^3 + 9*x^2 + 6*x + 9
 res_a = list_1[1][0] + list_2[0][0]
+res_c = result_num[-1]+result_num1[-1]
+print(res_c)
 
-print(f'Результат: {list_1[0][0]}*{list_1[0][1]}+{res_a}*{list_1[1][1]}')
-
-
-
-
+print(f'Результат: {list_1[0][0]}*{list_1[0][1]}+{res_a}*{list_1[1][1]}+{list_2[1][0]}*{list_2[1][1]}+{res_c}')
 
 
+# def max_sequence(list1):
+#     list1 = list(set(list1))
+#     res_list = []
+#     new_list = []
+#     while len(list1) > 0:
+#         min_value = min(list1)
+#         list1.remove(min_value)
+#         while True:
+#             new_list.append(min_value)
+#             min_value += 1
+#             if not (min_value in list1):
+#                 if len(new_list) > len(res_list):
+#                     res_list = new_list.copy()
+#                     new_list = []
+#                 break
+#             list1.remove(min_value)
+#     return res_list
 
+# def create_list(size):
+#     lst = []
+#     for i in range(size):
+#         lst.append(int(input(f'Введите {i + 1} число: ')))
+#     print(f'\nВаш список: {lst}')
+#     return lst
 
+# size = int(input('Введите размер списка: '))
+# user_list = create_list(size)
 
+# print(f'\nМаксимальная последовательность вашего списка: {max_sequence(user_list)}')
 
-
-
-
-# result_x2 = re.findall(r'\^.', s)
-# result_x2 = re.findall(r'\d+.x.', s)
-# result_x3 = re.findall(r'\*',s)
-
-
-
-
-# result = re.search(r'x.3', s) #<re.Match object; span=(2, 5), match='x^3'>
-# result = re.search(r'\d', s) #<re.Match object; span=(0, 1), match='5'>
-# result = re.search(r'\D', s) #<re.Match object; span=(1, 2), match='*'>
-# result = re.search(r'\d*', s) #<re.Match object; span=(0, 3), match='525'>
-
-# result = re.search(r'\d{1,1}', s) #<re.Match object; span=(0, 3), match='525'>
