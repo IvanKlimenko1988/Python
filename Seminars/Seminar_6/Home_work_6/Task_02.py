99cab838e86d90f3218da7d5c19a97e0cdb0b9c5
Ivan <i.klimenko.eng@gmail.com>
2022-11-15 13:27:03 +0400

    Task_02 changed

--- Seminars/Seminar_6/Home_work_6/Task_02.py
+++ Seminars/Seminar_6/Home_work_6/Task_02.py
@@ -1,18 +1,57 @@
 # Напишите программу вычисления арифметического выражения заданного строкой. Используйте операции +,-,/,*. приоритет операций стандартный.
-# *Пример:* 
-# 2+2 => 4; 
-# 1+2*3 => 7; 
+# *Пример:*
+# 2+2 => 4;
+# 1+2*3 => 7;
 # 1-2*3 => -5;
 # - Добавьте возможность использования скобок, меняющих приоритет операций.
-#     *Пример:* 
-#     1+2*3 => 7; 
+#     *Пример:*
+#     1+2*3 => 7;
 #     (1+2)*3 => 9;
+import re
+
+def find_digit(text):
+    numbers = re.compile('-?\d+')
+    list_digit = list(map(int, numbers.findall(text)))
+    return list_digit
 
 
+def find_operations(text):
+    pattern = '\D+'
+    list_operations = re.findall(pattern, text)
+    return list_operations
 
 
+def calculator(list_digit, list_oper):
+    for n in range(len(list_digit)):
+        for i in range(len(list_oper)):
+            if list_oper[i] == '*':
+                list_digit[i] *= list_digit[i + 1]
+                del list_digit[i + 1]
+                del list_oper[i]
+                break
+
+        for i in range(len(list_oper)):
+            if list_oper[i] == '/':
+                list_digit[i] /= list_digit[i + 1]
+                del list_digit[i + 1]
+                del list_oper[i]
+                break
+    return list_digit
 
 
+def result_operations(digits, operations):
+    if len(digits) == len(operations):
+        if operations[0] == '-':
+            del operations[0]
+    return operations
 
 
+expression = input("Введите арифметического выражения: ")
+expression = expression.replace(' ', '')
 
+digits = find_digit(expression)
+operations = find_operations(expression)
+operations = result_operations(digits, operations)
+result = sum(calculator(digits, operations))
+
+print(f'{expression} = {result}')
