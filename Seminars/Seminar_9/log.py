46896044f6f34127d367f9f4f964eaafe48e2288
Ivan <i.klimenko.eng@gmail.com>
2022-12-03 17:54:48 +0400

    Home Work finished

---
+++ Seminars/Seminar_9/log.py
@@ -0,0 +1,26 @@
+from datetime import datetime as dt
+from os import startfile
+
+def show_logs():
+    startfile("G:\Java\GeekBraints\Seminars\Python\logs.txt")
+    
+
+def logger(data_start, res):
+    time = dt.now().strftime('%H:%M')
+    with open('logs.txt', 'a') as file:
+        file.write(f'{time}: пользователь ввел {data_start}, ответ {res}\n')
+
+def logger_start():
+    time = dt.now().strftime('%H:%M')
+    with open('logs.txt', 'a') as file:
+        file.write(f'{time}: пользователь запустил программу\n')
+
+def logger_exit():
+    time = dt.now().strftime('%H:%M')
+    with open('logs.txt', 'a') as file:
+        file.write(f'{time}:  пользователь закрыл программу\n')
+
+def logger_btn(btn):
+    time = dt.now().strftime('%H:%M')
+    with open('logs.txt', 'a') as file:
+        file.write(f'{time}:  пользователь нажал кнопку {btn}\n')
