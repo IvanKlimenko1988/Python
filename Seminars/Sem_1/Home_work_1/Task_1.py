e42124599259de82aab24228c2b1ac3bdb897dfc
Ivan <i.klimenko.eng@gmail.com>
2023-01-27 19:32:08 +0400

    Task 1 finished

--- Seminars/Sem_1/Home_work_1/Task_1.py
+++ Seminars/Sem_1/Home_work_1/Task_1.py
@@ -1,3 +1,10 @@
 # Задача 1: Найдите сумму цифр трехзначного числа.
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+
+num = int(input("Введите трёхзначное число: "))
+hund = num // 100
+dozen = (num // 10) - (hund * 10)
+unit = num - num // 10 * 10
+resut = hund + dozen + unit
+print(f"Сумма чисел числа {num} -> {resut} ({hund} + {dozen} + {unit})")
