f7fc20830d2391027348926016937d1e52f95995
dev.klimenko <i.klimenko.eng@gmail.com>
2023-02-04 08:46:27 +0400

    Task 1 finished

--- Seminars/Sem_3/Home_work_3/Task_1.py
+++ Seminars/Sem_3/Home_work_3/Task_1.py
@@ -8,3 +8,19 @@
 # 1 2 3 4 5
 # 3
 # -> 1
+size = int(input("Введите количество элементов в массиве: "))
+start_list = [] * size
+numbers = 0
+
+while numbers < size:
+    start_list.append(int(input(f"Введите {numbers + 1} элемент массива: ")))
+    numbers += 1
+
+x = int(input("Введите искомый элемент Х: "))
+count = 0
+for i in range(len(start_list)):
+    if start_list[i] == x:
+        count += 1
+
+print(start_list)
+print(f"{x} -> {count}")
