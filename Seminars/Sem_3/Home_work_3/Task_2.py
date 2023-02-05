0eae5386e38ac5a5f31c61742bce27bc601b8de0
dev.klimenko <i.klimenko.eng@gmail.com>
2023-02-05 11:22:32 +0400

    Task 2 and Task 3 started

--- Seminars/Sem_3/Home_work_3/Task_2.py
+++ Seminars/Sem_3/Home_work_3/Task_2.py
@@ -8,3 +8,26 @@
 # 1 2 3 4 5
 # 6
 # -> 5
+
+# n = int(input("Введите количество элементов в массиве: "))
+# start_list = [] * n
+# numbers = 0
+#
+# while numbers < n:
+#     start_list.append(int(input(f"Введите {numbers + 1} элемент массива: ")))
+#     numbers += 1
+start_list = [10, 2, 15, 43, 4]
+x = int(input("Введите искомый элемент Х: "))
+
+check = False
+max_val = 0
+for i in range(len(start_list)):
+    if start_list[i] == x:
+        max_val = start_list[i]
+        check = True
+    elif (max_val < start_list[i]) & (check == False):
+        max_val = start_list[i]
+
+
+print(max_val)
+
--- Seminars/Sem_3/Home_work_3/Task_3.py
+++ Seminars/Sem_3/Home_work_3/Task_3.py
@@ -1,4 +1,4 @@
-# 3. В настольной игре Скрабл каждая буква имеент опеределенную ценность
+# 3. В настольной игре Скрабл каждая буква имеет определенную ценность
 # В случае с английским алфавитом очки распледеляюся так
 # A E I O U L N S T R  - 1 очко. D G - 2 очка. B C M P - 3 очка
 # F H V W Y - 4 очка. K - 5 очков. J X - 8 очков. Q Z - 10 очков
@@ -9,3 +9,63 @@
 # либо только русские либо латинские буквы
 
 # ноутбук - 12
+
+
+points_eng = {1: ["A", "E", "I", "O", "U", "L", "N", "S", "T", "R"],
+              2: ["D", "G"],
+              3: ["B", "C", "M", "P"],
+              4: ["F", "H", "V", "W", "Y"],
+              5: ["K"],
+              8: ["J", "X"],
+              10: ["Q", "Z"]}
+
+points_rus = {1: ["А", "В", "Е", "И", "Н", "О", "Р", "С", "Т"],
+              2: ["Д", "К", "Л", "М", "П", "У"],
+              3: ["Б", "Г", "Ё", "Ь", "Я"],
+              4: ["Й", "Ы"],
+              5: ["Ж", "З", "Х", "Ц", "Ч"],
+              8: ["Ш", "Э", "Ю"],
+              10: ["Ф", "Щ", "Ъ"]}
+
+word = "notebook".upper()
+list_char = []
+for i in word:
+    list_char.append(i)
+
+print(list_char)
+
+sum=0
+for i in list_char:
+    for k,v in points_rus.items():
+        if i in v:
+            sum+=k
+
+    for k,v in points_eng.items():
+        if i in v:
+            sum+=k
+
+
+
+# sum = 0
+# for key, value in points_rus.items():
+#     for i in list_char:
+#         if i in value:
+#             sum += key
+#         else:
+#             for key, value in points_eng.items():
+#                 for i in list_char:
+#                     if i in value:
+#                         sum += key
+
+
+
+# if list_char in points_rus.values():
+#     for key, value in points_eng.items():
+#         for i in list_char:
+#             if i in value:
+#                 sum += key
+
+
+    # print(f"{key} {value}")
+
+print(sum)
