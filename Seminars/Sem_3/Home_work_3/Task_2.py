62c5c81ba56c566cacb4ca20ba5aed44070a819b
dev.klimenko <i.klimenko.eng@gmail.com>
2023-02-08 16:59:40 +0400

    Task 2 finished

--- Seminars/Sem_3/Home_work_3/Task_2.py
+++ Seminars/Sem_3/Home_work_3/Task_2.py
@@ -9,25 +9,29 @@
 # 6
 # -> 5
 
-# n = int(input("Введите количество элементов в массиве: "))
-# start_list = [] * n
-# numbers = 0
-#
-# while numbers < n:
-#     start_list.append(int(input(f"Введите {numbers + 1} элемент массива: ")))
-#     numbers += 1
-start_list = [10, 2, 15, 43, 4]
+n = int(input("Введите количество элементов в массиве: "))
+start_list = [] * n
+numbers = 0
+
+while numbers < n:
+    start_list.append(int(input(f"Введите {numbers + 1} элемент массива: ")))
+    numbers += 1
+
 x = int(input("Введите искомый элемент Х: "))
 
-check = False
-max_val = 0
-for i in range(len(start_list)):
-    if start_list[i] == x:
-        max_val = start_list[i]
-        check = True
-    elif (max_val < start_list[i]) & (check == False):
-        max_val = start_list[i]
+print(f"Массив: {start_list}")
 
+if x in start_list:
+    print(f"Самое большое число к искомому {x} -> {x}")
+else:
+    list_result = []
+    for i in range(len(start_list)):
+        if x - start_list[i] < 0:
+            list_result.append((x - start_list[i]) * (-1))
+        else:
+            list_result.append(x - start_list[i])
+    min_diff = min(list_result)
+    result_index = list_result.index(min_diff)
+    print(f"Самое большое число к искомому {x} -> {start_list[result_index]}")
 
-print(max_val)
 
