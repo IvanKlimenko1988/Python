63adaea8bc4f70fc77365cf6a5ceab348f32f1d4
Ivan <i.klimenko.eng@gmail.com>
2023-02-25 12:18:27 +0400

    Task finished

--- Seminars/Sem_8/PhoneBook.py
+++ Seminars/Sem_8/PhoneBook.py
@@ -14,8 +14,10 @@
 # Дополнить телефонный справочник возможностью изменения и удаления данных.
 # Пользователь также может ввести имя или фамилию, и Вы должны реализовать
 # функционал для изменения и удаления данных
-from random import *
 import json
+
+phone_number = {}
+persons = []
 
 file_bd = "G:\Java\GeekBraints\Seminars\Python\Seminars\Sem_8\PB.json"
 
@@ -33,63 +35,6 @@ def print_2d_list(array):
     for i in range(len(array)):
         print(''.join(map(str, array[i])))
 
-def main():
-    print('"Телефонная книга"')
-    run = True
-    while run:
-        print("Доступные команды:")
-        print_2d_list(com_list)
-        command = input("Введите команду: ")
-        if command == "1":
-            print("Текущий список контактов:")
-            if len(phone_number) == 0:
-                print("Список контактов пуст")
-            else:
-                for key, value in phone_number.items():
-                    print(f"Телефон: {key}: {value}")      
-        elif command == "2":
-            name = input('Введите телефон: ')
-            if name in phone_number:
-                    print("Taкой телефон уже есть в книге")
-            else:
-                phone_number[name] = []
-                print('Телефон успешно добавлен!')
-        elif command == "3":
-            print("Выберете номер телефона, для которого хотите добавить данные: ")
-            name = input("Номер телефона: ")
-            if name in phone_number:
-                person_name = input("Введите данные (ФИО): ")
-                phone_number[name] = [person_name]
-                print('Данные успешно добавлены!')     
-            else:
-                print("Такого телефона в книге нет!")
-        elif command == "4":
-            print("Вы действительно хотите удалить телефон?")
-            answer = input("Ведите да или нет: ").lower()
-            print(answer)
-            if answer == "да":
-                del_num = input('Введите номер телефона: ')
-                if del_num in phone_number:
-                    del phone_number[del_num]
-                    print(f"Контакт с номером {del_num} удален")
-            else:
-                print("Такого номера нет в книге!")
-        elif command == "5":
-            save()
-            print("Изменения успешно сохранены!")
-        elif command == "6":
-            phone_number = load()
-            print('Книга контактов успешно загружена!')
-        elif command == "7":
-            save()
-            print("Программа завершила свою роботу.")
-            run = False     
-        elif command == "help":
-            print_2d_list(com_list)
-        else:
-            print("Неопознная команда. Используйте справку help")
-
-
 com_list = ["/menu - список всех комманд", 
             "1 - общий список контактов", 
             "2 - добавить телефон", 
@@ -99,7 +44,58 @@ com_list = ["/menu - список всех комманд",
             "6 - загрузить базу контактов",
             "7 - завершение программы"]
 
-phone_number = {}
-persons = []
+print('"Телефонная книга"')
+run = True
+while run:
+    print("Доступные команды:")
+    print_2d_list(com_list)
+    command = input("Введите команду: ")
+    if command == "1":
+        print("Текущий список контактов:")
+        if len(phone_number) == 0:
+            print("Список контактов пуст")
+        else:
+            for key, value in phone_number.items():
+                print(f"Телефон: {key}: {value}")      
+    elif command == "2":
+        name = input('Введите телефон: ')
+        if name in phone_number:
+                print("Taкой телефон уже есть в книге")
+        else:
+            phone_number[name] = []
+            print('Телефон успешно добавлен!')
+    elif command == "3":
+        print("Выберете номер телефона, для которого хотите добавить данные: ")
+        name = input("Номер телефона: ")
+        if name in phone_number:
+            person_name = input("Введите данные (ФИО): ")
+            phone_number[name] = [person_name]
+            print('Данные успешно добавлены!')     
+        else:
+            print("Такого телефона в книге нет!")
+    elif command == "4":
+        print("Вы действительно хотите удалить телефон?")
+        answer = input("Ведите да или нет: ").lower()
+        print(answer)
+        if answer == "да":
+            del_num = input('Введите номер телефона: ')
+            if del_num in phone_number:
+                del phone_number[del_num]
+                print(f"Контакт с номером {del_num} удален")
+        else:
+            print("Такого номера нет в книге!")
+    elif command == "5":
+        save()
+        print("Изменения успешно сохранены!")
+    elif command == "6":
+        phone_number = load()
+        print('Книга контактов успешно загружена!')
+    elif command == "7":
+        save()
+        print("Программа завершила свою роботу.")
+        run = False     
+    elif command == "help":
+        print_2d_list(com_list)
+    else:
+        print("Неопознная команда. Используйте справку help")
 
-main()
